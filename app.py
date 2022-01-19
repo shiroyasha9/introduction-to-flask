3e9829ffaaede97470ab2e9e06bf76ccc89667f8
Mubin Ansari <mubinansari910@gmail.com>
2022-01-20 01:28:54 +0530

    add dynamic route example

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def information():
     return "<h1>Cats are awesome!</h1>"
 
 
+@app.route("/cat/<name>")
+def greet(name):
+    return f"<h1>This is a page for {name} with all caps is {name.upper()}!</h1>"
+
+
 if __name__ == "__main__":
     app.run()
 
