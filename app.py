d0d545491ff99602dcbc705a24a5bcf2fd9efb97
Mubin Ansari <mubinansari910@gmail.com>
2022-01-20 01:37:52 +0530

    implement basic template

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello Cat!</h1>"
-
-
-@app.route("/information")
-def information():
-    return "<h1>Cats are awesome!</h1>"
-
-
-@app.route("/cat/<name>")
-def greet(name):
-    return f"<h1>100th letter {name[100]}</h1>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# first show the error without the debug = True, then with it.
-# copy debug pin and show how to use console on web
