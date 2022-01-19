7b0811152c7ed66681a8afffe89c7b14ba9669ff
Mubin Ansari <mubinansari910@gmail.com>
2022-01-20 01:52:57 +0530

    add example for template control flow

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    name = "Ginger"
-    letters = list(name)
-    cat_dictionary = {"cat_name": "Ginger"}
-    return render_template(
-        "index.html", name=name, letters=letters, cat_dictionary=cat_dictionary
-    )
+    mylist = [1, 2, 3, 4, 5, 6]
+    cats = ["Ginger", "Tom", "Leo"]
+    return render_template("index.html", mylist=mylist, cats=cats)
+
+
+@app.route("/secret")
+def secret():
+    user_logged_in = False
+    return render_template("secret.html", user_logged_in=user_logged_in)
 
 
 if __name__ == "__main__":
