c3df52fbc0f9114b4774793c9ff1dc2094a5d8df
Mubin Ansari <mubinansari910@gmail.com>
2022-01-20 01:21:28 +0530

    add basic routing

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def index():
     return "<h1>Hello Cat!</h1>"
 
 
+@app.route("/information")
+def information():
+    return "<h1>Cats are awesome!</h1>"
+
+
 if __name__ == "__main__":
     app.run()
 
