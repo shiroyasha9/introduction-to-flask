cc1fb5f3dff27041b10b3e55cdc5490b2f7c12ae
Mubin Ansari <mubinansari910@gmail.com>
2022-01-21 12:07:03 +0530

    add example for template inheritance

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    mylist = [1, 2, 3, 4, 5, 6]
-    cats = ["Ginger", "Tom", "Leo"]
-    return render_template("index.html", mylist=mylist, cats=cats)
+    return render_template("index.html")
 
 
-@app.route("/secret")
-def secret():
-    user_logged_in = False
-    return render_template("secret.html", user_logged_in=user_logged_in)
+@app.route("/cat/<name>")
+def cat_name(name):
+    return render_template("cat.html", name=name)
 
 
 if __name__ == "__main__":
