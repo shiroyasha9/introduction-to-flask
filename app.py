c04505b6c672c28ce7a26ac585fb79367252ad06
Mubin Ansari <mubinansari910@gmail.com>
2022-01-21 12:30:03 +0530

    add form example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,21 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/cat/<name>")
-def cat_name(name):
-    return render_template("cat.html", name=name)
+@app.route("/signup")
+def signup_form():
+    return render_template("signup.html")
+
+
+@app.route("/thankyou")
+def thankyou_form():
+    first = request.args.get("first")
+    last = request.args.get("last")
+    return render_template("thankyou.html", first=first, last=last)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
